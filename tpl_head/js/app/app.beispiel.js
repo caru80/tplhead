e3ce90993136bce1769ee9897d8492283cb0a63f
Carsten <c.ruppert@headmarketing.de>
2018-02-05 14:02:38 +0100

    Aufgeräumt.

--- tpl_head/js/app/app.beispiel.js
+++ tpl_head/js/app/app.beispiel.js
@@ -1,16 +1,11 @@
+
 'use strict';
 
 /**
-	JavaScript Cookie v2.2.0
-	https://github.com/js-cookie/js-cookie
-
-	Copyright 2006, 2015 Klaus Hartl & Fagner Brack
-	Released under the MIT license
+	Cookies v2.1.4
+	From https://github.com/js-cookie/js-cookie
 */
-(function(m){var h=!1;"function"===typeof define&&define.amd&&(define(m),h=!0);"object"===typeof exports&&(module.exports=m(),h=!0);if(!h){var e=window.Cookies,a=window.Cookies=m();a.noConflict=function(){window.Cookies=e;return a}}})(function(){function m(){for(var e=0,a={};e<arguments.length;e++){var b=arguments[e],c;for(c in b)a[c]=b[c]}return a}function h(e){function a(b,c,d){if("undefined"!==typeof document){if(1<arguments.length){d=m({path:"/"},a.defaults,d);if("number"===typeof d.expires){var k=
-new Date;k.setMilliseconds(k.getMilliseconds()+864E5*d.expires);d.expires=k}d.expires=d.expires?d.expires.toUTCString():"";try{var g=JSON.stringify(c);/^[\{\[]/.test(g)&&(c=g)}catch(p){}c=e.write?e.write(c,b):encodeURIComponent(String(c)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent);b=encodeURIComponent(String(b));b=b.replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent);b=b.replace(/[\(\)]/g,escape);g="";for(var l in d)d[l]&&(g+="; "+l,!0!==d[l]&&(g+="="+
-d[l]));return document.cookie=b+"="+c+g}b||(g={});l=document.cookie?document.cookie.split("; "):[];for(var h=/(%[0-9A-Z]{2})+/g,n=0;n<l.length;n++){var q=l[n].split("="),f=q.slice(1).join("=");this.json||'"'!==f.charAt(0)||(f=f.slice(1,-1));try{k=q[0].replace(h,decodeURIComponent);f=e.read?e.read(f,k):e(f,k)||f.replace(h,decodeURIComponent);if(this.json)try{f=JSON.parse(f)}catch(p){}if(b===k){g=f;break}b||(g[k]=f)}catch(p){}}return g}}a.set=a;a.get=function(b){return a.call(a,b)};a.getJSON=function(){return a.apply({json:!0},
-[].slice.call(arguments))};a.defaults={};a.remove=function(b,c){a(b,"",m(c,{expires:-1}))};a.withConverter=h;return a}return h(function(){})});d,d.get=function(a){return d.call(d,a)},d.getJSON=function(){return d.apply({json:!0},[].slice.call(arguments))},d.defaults={},d.remove=function(b,c){d(b,"",a(c,{expires:-1}))},d.withConverter=b,d}return b(function(){})});
+!function(a){let b=!1;if("function"==typeof define&&define.amd&&(define(a),b=!0),"object"==typeof exports&&(module.exports=a(),b=!0),!b){let c=window.Cookies,d=window.Cookies=a();d.noConflict=function(){return window.Cookies=c,d}}}(function(){function a(){for(let a=0,b={};a<arguments.length;a++){let c=arguments[a];for(let d in c)b[d]=c[d]}return b}function b(c){function d(b,e,f){let g;if("undefined"!=typeof document){if(arguments.length>1){if(f=a({path:"/"},d.defaults,f),"number"==typeof f.expires){let h=new Date;h.setMilliseconds(h.getMilliseconds()+864e5*f.expires),f.expires=h}f.expires=f.expires?f.expires.toUTCString():"";try{g=JSON.stringify(e),/^[\{\[]/.test(g)&&(e=g)}catch(p){}e=c.write?c.write(e,b):encodeURIComponent(e+"").replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent),b=encodeURIComponent(b+""),b=b.replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent),b=b.replace(/[\(\)]/g,escape);let i="";for(let j in f)f[j]&&(i+="; "+j,!0!==f[j]&&(i+="="+f[j]));return document.cookie=b+"="+e+i}b||(g={});for(let k=document.cookie?document.cookie.split("; "):[],l=0;l<k.length;l++){let m=k[l].split("="),n=m.slice(1).join("=");'"'===n.charAt(0)&&(n=n.slice(1,-1));try{let o=m[0].replace(/(%[0-9A-Z]{2})+/g,decodeURIComponent);if(n=c.read?c.read(n,o):c(n,o)||n.replace(/(%[0-9A-Z]{2})+/g,decodeURIComponent),this.json)try{n=JSON.parse(n)}catch(p){}if(b===o){g=n;break}b||(g[o]=n)}catch(p){}}return g}}return d.set=d,d.get=function(a){return d.call(d,a)},d.getJSON=function(){return d.apply({json:!0},[].slice.call(arguments))},d.defaults={},d.remove=function(b,c){d(b,"",a(c,{expires:-1}))},d.withConverter=b,d}return b(function(){})});
 
 /**
 	Polyfill für Object.keys()
@@ -150,16 +145,14 @@ Object.keys||(Object.keys=function(){let e=Object.prototype.hasOwnProperty,f=!{t
 				this.request(ext, name);
 			},
 
-			/** Gibt etwas in der Konsole aus: */
-			log : function(msg, type) {
+			/** Gib etwas in der Konsole aus: */
+			log : function(type, file) {
 				let msg = '';
 
 				switch(type) {
 					case 'err'
-						msg = 'Fehler: ' + msg + ' konnte nicht geladen werden.';
+						msg = 'Fehler: ' + file + ' konnte nicht geladen werden.';
 					break;
-					default :
-						msg = 'Hinweis: ' + msg;
 				}
 
 				if(console) console.log(msg);
@@ -296,17 +289,33 @@ Object.keys||(Object.keys=function(){let e=Object.prototype.hasOwnProperty,f=!{t
 	*/
 	$app.extensions.list = {
 		/**
-			emptyTemplate : {
-				autoload	: true
-				file		: 'templates/head/js/',
-				options		: {},
-				//condition 	: function() {},
-				error		: function() { $app.extensions.log('err', this.file); },
-				success		: function() {
-
+			foo : { 									Beliebiger Objektname
+				autoload	: true						Automatisch laden (Ein/Aus).
+				(,available 	: false) 				Wird nach dem Laden gesetzt und sagt aus ob das Script verfügbar ist.
+				,file 		: 'templates/js/foo.js'		Der Pfad zur Datei. Grundsätzlich werden nur Dateien vom selben Ursprung (vom selben Server) geladen.
+				[,options 	: {}] 						Optionen auf die man zugreifen könnte via: $app.extebnsions.list.foo.options.meineOption
+				,condition 	: function()				Eine weitere Kontrollstruktur, die das laden von "foo" erlaubt, oder auch nicht.
+				{
+					return false; 	= Dieses Script darf nicht geladen werden, weil die Bedingung nicht erfüllt wurde!
+					return true; 	= Dieses Script darf geladen werden, weil die Bedingung erfüllt wurde!
 				}
-			},
+				,error		: function()
+				{
+					// Script konnte nicht geladen werden.
+				}
+				,success 	: function()
+				{
+					// Script wurde geladen und steht zur Verfügung.
+				}
+			}
+
+			// Das erfolgreiche laden von "foo" löst an $app den Event "fooReady" aus, den man wie folgt nutzen könnte:
+
+			$(window).one('appReady', function() { // $app muss Verfügbar sein.
+				$($app).one('fooReady', function() {...}); // "foo" wurde geladen.
+			});
 		*/
+
 
 		/**
 			Systemnachrichten
@@ -334,26 +343,15 @@ Object.keys||(Object.keys=function(){let e=Object.prototype.hasOwnProperty,f=!{t
 		},
 
 		/**
-			AJAX
-		*/
-		ajax : {
-			autoload	: false
-			,file 		: 'templates/head/js/app/app.ajax.js'
-			,error		: function() { $app.extensions.log('err', this.file); }
-			,success	: function() {
-				$app.ajax.init();
-			}
-		},
-
-		/**
 			Google Analytics
+
 			Der Autoload kann immer an bleiben, solange kein Key eingegeben wurde macht das gar nichts (siehe: condition).
 		*/
 		ganalytics : {
 			autoload	: true
 			,file 		: 'templates/head/js/app/app.ganalytics.js'
 			,options	:   {
-								key 		: '', 			// Seitenschlüssel / „Property-Key”
+								key 		: '', 			// Seitenschlüssel / Key
 								cookiename 	: 'gaoptout' 	// Name des Cookies, der das Opt-Out des Users speichert.
 							}
 			,condition	: function()
@@ -366,15 +364,169 @@ Object.keys||(Object.keys=function(){let e=Object.prototype.hasOwnProperty,f=!{t
 			}
 			,error		: function() { $app.extensions.log('err', this.file); }
 			,success	: function() {
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-				ga('create', $app.extensions.list.ganalytics.options.key, 'auto');
-				ga('set', 'anonymizeIp', true);
-				ga('send', 'pageview');
-				$app.extensions.log('Google Analytics wurde gestartet.');
+				if(console) console.log('$app notice: Google Analytics loaded.');
 			}
 		},
-	}/*********** Ende Liste */
+
+		/**
+			AJAX
+		*/
+		ajax : {
+			autoload	: false
+			,file 		: 'templates/head/js/app/app.ajax.js'
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success	: function() {
+				$app.ajax.init();
+			}
+		},
+
+		/**
+			$app.equalColumns
+		*/
+		equalColumns : {
+			autoload	: false
+			,file 		: 'templates/head/js/app/app.equalcols.js'
+			,options	: {}
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success	: function() {
+				// Das Ding kann jederzeit neu abgefeuert werden. Wenn z.B. neue Dinge per AJAX geladen wurden.
+				$app.equalColumns.init(this.options)
+			}
+		},
+
+		/**
+			Sticky
+		*/
+		sticky : {
+			autoload	: false
+			,file 		: 'templates/head/js/sticky.js'
+			,options	: {}
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success	: function() {
+				$('[data-sticky]').each(function() {
+					$(this).sticky($app.extensions.list.sticky.options);
+				});
+			}
+		},
+
+		/*
+			Joomla! Suchindex
+		*/
+		finder: {
+			autoload	: false
+			,file 		: 'templates/head/js/app/app.finder.js'
+			,options	: { form : '#mod-finder-searchform', queryinput : '#mod-finder-searchword', results : '#search-results' }
+			,condition	: function()
+			{
+				if( $(this.options.form).length === 0 ) return false; // Es ist keine Suchmaske vorhanden.
+				return true;
+			}
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success 	: function() {
+				$app.finder.init( this.options );
+			}
+		},
+		/*
+			Joomla! Suchindex Autocompleter
+			Wird bei Bedarf (wenn im Modul eingeschaltet) von $app.finder nachgeladen
+		*/
+		finderAutocompleter : {
+			enable 	: false // Das soll nicht eingeschaltet werden, weil es bei Bedarf nachgeladen wird.
+			,file	: 'media/jui/js/jquery.autocomplete.min.js'
+			,error	: function() { $app.extensions.log('err', this.file); }
+		},
+
+		/*
+			Wegpunkte (One-Page-Navigation)
+		*/
+		jqueryNav : {
+			autoload	: false
+			,file 		: 'templates/head/js/jquery.nav.js'
+			,error		: function() { $app.extensions.log('err', this.file); }
+		},
+		waypoints : {
+			autoload	: false
+			,file 		: 'templates/head/js/app/app.waypoints.js'
+			,options	: {node : '#waypoints', targets : 'section', ignore : '', offsetElement : '', topId : 'top'}
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success	: function() {
+				let initNav = function() {
+					$app.waypoints.init($.extend({makeNew : true}, this.options));
+				}
+				if(!$app.extensions.list.jqueryNav.available) { // Wir brauchen jquery.nav.js, aber es ist noch nicht geladen
+					$($app).one('jqueryNavReady', function() {
+						initNav();
+					});
+					$app.extensions.load('jqueryNav', true);
+				}
+				else {
+					initNav();
+				}
+			}
+		},
+
+		/*
+			Protoslider
+		*/
+		protoslider : {
+			autoload	: false
+			,file 		: 'templates/head/js/protoslider.min.js'
+			,condition	: function()
+			{
+				if ($('.ptslider').length) return true; // Nur laden, wenn ein Element mit Klasse ptslider da ist
+				return false;
+			}
+			,error		: function() { $app.extensions.log('err', this.file); }
+			,success	: function() {
+				/*
+					Beispiel Heroslider
+				let heroslider = $('.ptslider.heroslider').protoslider({
+					timeout 		: 5000,
+					autoheight		: false,
+					pauseonhover 	: false,
+					navigation 		: false,
+
+					// Bilder im Vordergrund nach dem Preload als Hintergrundbild in das jeweilige Item einfügen, und dann aus dem DOM entfernen.
+					onAfterImagePreload : function()
+					{
+						for( let i = 0, len = this.$slides.length; i < len; i++ )
+						{
+							let slide 	= this.$slides.eq(i),
+								img		= slide.children('img');
+
+							if( img.length )
+							{
+								slide.css({'background-image':'url(\''+img.get(0).src+'\')'});
+								img.remove();
+							}
+						}
+					},
+					onAfterInit : function()
+					{
+						this.State.stage[0].find('.caption').addClass('show'); // 1. Caption einblenden (Klasse .show)
+					}
+				});
+				*/
+
+				/*
+					Captions ein-/ausblenden (Klasse .show)
+				heroslider.on('afterSlideIn.app.protoslider', function(ev, d){
+					d.slide.find('.caption').addClass('show');
+				}).on('afterSlideOut.app.protoslider', function(ev, d){
+					d.slide.find('.caption').removeClass('show');
+				});
+				*/
+
+				/*
+					Alle Protoslider, außer .heroslider, starten.
+					$('.ptslider').not('.ptslider.heroslider').each(function()
+					{
+						$(this).protoslider( $app.extensions.list.protoslider.options );
+					});
+				*/
+			}
+		}
+	}
+	/*********** Ende Liste */
+
 })(jQuery);
