a9d60440fb5651fac693f11ee92a46a571a9eeed
Carsten <c.ruppert@headmarketing.de>
2019-01-31 17:00:46 +0100

    Fix Youtube und Lokale Videos

--- tpl_head/subforms/accordion/accordion.php
+++ tpl_head/subforms/accordion/accordion.php
@@ -9,7 +9,6 @@
 	$a = 0; // Zähler Accordions
 	$c = 0; // Zähler Items
 ?>
-<!--div class="accordion" id="item-page-accordion" role="tablist" aria-multiselectable="true"-->
 <?php
 	foreach($list as $i => $field) :
 		
@@ -35,7 +34,7 @@
 		$closed     = (bool) !empty($field->enhanced_closed) && $enh ? $field->enhanced_closed : false;
 ?>
 			<div class="card panel-<?php echo $c;?><?php echo $classList;?>">
-				<div class="card-header" id="heading-<?php echo $c;?>" role="tab" 
+				<div class="card-header" id="heading-panel-<?php echo $c;?>" role="tab" 
 					data-toggle="collapse" 
 					data-target="#panel-content-<?php echo $c;?>" 
 					aria-expanded="<?php echo $closed != true ? 'true' : 'false';?>"
@@ -48,6 +47,53 @@
 					aria-labelledby="heading-<?php echo $c;?>" 
 					data-parent="#item-page-accordion-<?php echo $a;?>"
 				>
+					<?php
+						if(!empty($field->panel_introimage)):
+					?>
+							<img src="<?php echo $field->panel_introimage;?>" class="card-img-top" alt="<?php echo !empty($field->panel_introimage_alt) ? $field->panel_introimage_alt : '';?>">
+					<?php
+						endif;
+					?>
+
+					<?php
+						if(!empty($field->panel_video_yt)):
+					?>
+						<div class="panel-video-wrapper ytube">					
+							<iframe class="ytube" src="https://www.youtube-nocookie.com/embed/<?php echo $field->panel_video_yt;?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							<style>
+								.panel-video-wrapper.ytube {
+									position: relative;
+									padding-top: 56.25%;
+									height: 0;
+									overflow: hidden;
+
+								}
+								.panel-video-wrapper iframe.ytube {
+									position: absolute;
+									top: 0;
+									left: 0;
+									width: 100%;
+									height: 100%;
+								}
+							</style>
+						</div>
+					<?php
+						endif;
+					?>
+
+					<?php
+						if(!empty($field->panel_video_local) && $field->panel_video_local != '-1'):
+					?>
+						<div class="panel-video-wrapper">
+							<video controls class="panel-video" style="width: 100%;">
+								<source src="<?php echo \Joomla\CMS\Uri\Uri::root() . 'images/videos/' . $field->panel_video_local;?>" type="video/mp4">
+							</video>
+						</div>
+					<?php
+						endif;
+					?>
+
+
 					<div class="card-body">
 						<?php 
 							// Content Events auslösen
